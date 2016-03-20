ee66f2dbcd8a96a3cc2ed4f5dfaf64a29155ce37
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-03-20 15:45:05 -0700

    able to get a winner

--- js/app.js
+++ js/app.js
@@ -4,22 +4,53 @@ $(document).ready(function()  {
 
 
   var clickCount = 0;
-  var playerX = 0;
-  var playerY = 0;
+  var playerX = "X";
+  var playerY = "O";
 
+  // var $boxes = [[$('#1'), $('#2'), $('#3')],
+  //              [$('#4'), $('#5'), $('#6')],
+  //              [$('#7'), $('#8'), $('#9')]];
+
+  var $boxes = [];
+  console.log($boxes);
 
   $('.box').click(function(event){
     if( !$(this).text() ){
       if (clickCount % 2 === 0 ){
-        $(this).text("X");
+        $(this).text(playerX);
           clickCount ++;
       }else if(clickCount % 2 !== 0){
-          $(this).text("O");
+          $(this).text(playerY);
           clickCount ++;
         }
     }
-  });
+    if ( $('#1')||$('#2')||$('#3')||$('#4')||$('#5')||$('#6')||$('#7')||$('#8')||$('#9') ){
+        $boxes.unshift(this);
+        console.log($boxes);
+    }
 
+    var hLine1 = $('#1').text() === "X" && $('#2').text() === "X" && $('#3').text() === "X" || $('#1').text() === "O" && $('#2').text() === "O" && $('#3').text() === "O";
+    var hLine2 = $('#4').text() === "X" && $('#5').text() === "X" && $('#6').text() === "X" || $('#4').text() === "O" && $('#5').text() === "O" && $('#6').text() === "O";
+    var hLine3 = $('#7').text() === "X" && $('#8').text() === "X" && $('#9').text() === "X" || $('#7').text() === "O" && $('#8').text() === "O" && $('#9').text() === "O";
+    var vLine1 = $('#1').text() === "X" && $('#4').text() === "X" && $('#7').text() === "X" || $('#1').text() === "O" && $('#4').text() === "O" && $('#7').text() === "O";
+    var vLine2 = $('#2').text() === "X" && $('#5').text() === "X" && $('#8').text() === "X" || $('#2').text() === "O" && $('#5').text() === "O" && $('#8').text() === "O";
+    var vLine3 = $('#3').text() === "X" && $('#6').text() === "X" && $('#9').text() === "X" || $('#3').text() === "O" && $('#6').text() === "O" && $('#9').text() === "O";
+    var dLine1 = $('#1').text() === "X" && $('#5').text() === "X" && $('#9').text() === "X" || $('#1').text() === "O" && $('#5').text() === "O" && $('#9').text() === "O";
+    var dLine2 = $('#3').text() === "X" && $('#5').text() === "X" && $('#7').text() === "X" || $('#3').text() === "O" && $('#5').text() === "O" && $('#7').text() === "O";
+
+    if ( hLine1 || hLine2 || hLine3 || vLine1 || vLine2 || vLine3 || dLine1 || dLine2){
+        alert("You Won!");
+     }
+  });
+  // if(hLine1 || hLine2 || hLine3 || vLine1 || vLine2 || vLine3 || dLine1 || dLine2){
+  //
+  //   alert("winner");
+  // }
+
+
+
+
+  //hline1 || hLine2 || hLine3 || vLine1 || vLine2 || vLine3 || dLine1 || dLine2
 
 
 
@@ -27,6 +58,7 @@ $(document).ready(function()  {
 
   $('.btn').on("click", function (){
       $(".box").text("");
+      $boxes = [];
   });
 
 
@@ -38,7 +70,3 @@ $(document).ready(function()  {
 //player X chooses a square. You want to record X.
 //X cannot be chosen again
 //player O chooses
-
-var arrBoxes = [[null, null, null],
-                [null, null, null],
-                [null, null, null]];
