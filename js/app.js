8b8308f90db78a16e1bc9ee9fac44295a80060f9
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-03-19 17:00:53 -0700

    inserted X&O and denied override

--- js/app.js
+++ js/app.js
@@ -1,6 +1,40 @@
 // wait for the DOM to finish loading
-$(document).ready(function() {
+$(document).ready(function()  {
   // all code to manipulate the DOM
+
+
+  var clickCount = 0;
+  var playerX = 0;
+  var playerY = 0;
+
+
+  $('.box').click(function(event){
+    if( !$(this).text() ){
+      if (clickCount % 2 === 0 ){
+        $(this).text("X");
+          clickCount ++;
+      }else if(clickCount % 2 !== 0){
+          $(this).text("O");
+          clickCount ++;
+
+      }
+    }
+  });
+
+  $('btn').on("click", function (){
+    clickCount = 0;
+  });
+
+
+
+
   // goes inside this function
 
 });
+//player X chooses a square. You want to record X.
+//X cannot be chosen again
+//player O chooses
+
+var arrBoxes = [[null, null, null],
+                [null, null, null],
+                [null, null, null]];
