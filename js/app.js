b8270e974ecd249e6d8a1fab5debd87d8932a683
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-03-21 07:41:55 -0700

    final design

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ $(document).ready(function()  {
   // all code to manipulate the DOM
   //variable used to keep track whos turn it is % who won
   var clickCount = 0;
+  var player1 = "1";
+  var player0 = "0";
   //A array jus to keep track of moves
   var $boxes = [];
   //lets me know whats being clicked
@@ -14,10 +16,12 @@ $(document).ready(function()  {
   $('.box').click(function(event){
     if( !$(this).text() ){
       if (clickCount % 2 === 0 ){
-        $(this).text("X");
+        $(this).text("1");
+        $(this).addClass("1");
           clickCount ++;    //add a count so player O can go
       }else if(clickCount % 2 !== 0){
-          $(this).text("O");
+          $(this).text("0");
+          $(this).addClass("0");
           clickCount ++;
         }
     }
@@ -28,24 +32,24 @@ $(document).ready(function()  {
     }
 
     //conditions to win
-      var hLine1 = $('#1').text() === "X" && $('#2').text() === "X" && $('#3').text() === "X" || $('#1').text() === "O" && $('#2').text() === "O" && $('#3').text() === "O";
-      var hLine2 = $('#4').text() === "X" && $('#5').text() === "X" && $('#6').text() === "X" || $('#4').text() === "O" && $('#5').text() === "O" && $('#6').text() === "O";
-      var hLine3 = $('#7').text() === "X" && $('#8').text() === "X" && $('#9').text() === "X" || $('#7').text() === "O" && $('#8').text() === "O" && $('#9').text() === "O";
-      var vLine1 = $('#1').text() === "X" && $('#4').text() === "X" && $('#7').text() === "X" || $('#1').text() === "O" && $('#4').text() === "O" && $('#7').text() === "O";
-      var vLine2 = $('#2').text() === "X" && $('#5').text() === "X" && $('#8').text() === "X" || $('#2').text() === "O" && $('#5').text() === "O" && $('#8').text() === "O";
-      var vLine3 = $('#3').text() === "X" && $('#6').text() === "X" && $('#9').text() === "X" || $('#3').text() === "O" && $('#6').text() === "O" && $('#9').text() === "O";
-      var dLine1 = $('#1').text() === "X" && $('#5').text() === "X" && $('#9').text() === "X" || $('#1').text() === "O" && $('#5').text() === "O" && $('#9').text() === "O";
-      var dLine2 = $('#3').text() === "X" && $('#5').text() === "X" && $('#7').text() === "X" || $('#3').text() === "O" && $('#5').text() === "O" && $('#7').text() === "O";
+      var hLine1 = $('#1').text() === "1" && $('#2').text() === "1" && $('#3').text() === "1" || $('#1').text() === "0" && $('#2').text() === "0" && $('#3').text() === "0";
+      var hLine2 = $('#4').text() === "1" && $('#5').text() === "1" && $('#6').text() === "1" || $('#4').text() === "0" && $('#5').text() === "0" && $('#6').text() === "0";
+      var hLine3 = $('#7').text() === "1" && $('#8').text() === "1" && $('#9').text() === "1" || $('#7').text() === "0" && $('#8').text() === "0" && $('#9').text() === "0";
+      var vLine1 = $('#1').text() === "1" && $('#4').text() === "1" && $('#7').text() === "1" || $('#1').text() === "0" && $('#4').text() === "0" && $('#7').text() === "0";
+      var vLine2 = $('#2').text() === "1" && $('#5').text() === "1" && $('#8').text() === "1" || $('#2').text() === "0" && $('#5').text() === "0" && $('#8').text() === "0";
+      var vLine3 = $('#3').text() === "1" && $('#6').text() === "1" && $('#9').text() === "1" || $('#3').text() === "0" && $('#6').text() === "0" && $('#9').text() === "0";
+      var dLine1 = $('#1').text() === "1" && $('#5').text() === "1" && $('#9').text() === "1" || $('#1').text() === "0" && $('#5').text() === "0" && $('#9').text() === "0";
+      var dLine2 = $('#3').text() === "1" && $('#5').text() === "1" && $('#7').text() === "1" || $('#3').text() === "0" && $('#5').text() === "0" && $('#7').text() === "0";
 
       // if theres a winner, check what count you're on. If even,
       // player O wins. Vice versa if false
       //if no winner, and count is to 9, then its a draw
       if ( hLine1 || hLine2 || hLine3 || vLine1 || vLine2 || vLine3 || dLine1 || dLine2){
           if (clickCount % 2 === 0){
-            alert("PLAYER O WON!");
+            alert("PLAYER 0 WON!");
           }
           else {
-            alert("PLAYER X WON!");
+            alert("PLAYER 1 WON!");
           }
       }
       else if ( clickCount === 9){
@@ -53,7 +57,6 @@ $(document).ready(function()  {
       }
 
   });
-
   //reset button. When clicked, make each box blank.
   //make clickCount = 0 so turns wont get out of whack
   $('.btn').on("click", function (){
@@ -61,11 +64,6 @@ $(document).ready(function()  {
       $boxes = [];
       clickCount = 0;
   });
-
-
-
-
-
 
   // goes inside this function
 
